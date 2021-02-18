7381be337648861f8245ec3d6ed2d1f9dea617c9
Mandip Malviya <mandipmalaviya@gmail.com>
2021-02-18 10:57:29 +0530

    add new theme

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,5 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
 
@@ -9,20 +9,75 @@
 
 </head>
 
-<body>
-    <div class="container">
-        <header class="row">
-            @include('includes.header')
-        </header>
-        <div id="main" class="row">
-            @yield('content')
+<body id="page-top">
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+    <!-- Sidebar -->
+@include('includes.sidebar')
+<!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+        @include('includes.header')
+        <!-- End of Topbar -->
+
+            <!-- Begin Page Content -->
+        @yield('content')
+        <!-- /.container-fluid -->
+
         </div>
-        <footer class="row">
-            @include('includes.footer')
-            
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; Shinecorner 2021</span>
+                </div>
+            </div>
         </footer>
-        
+        <!-- End of Footer -->
+
     </div>
+    <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-primary" href="login.html">Logout</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+@include('includes.footer')
+
+
 </body>
 
 </html>
